782280c77a742b514d2236ca06c6e13e46ba7052
DamienDeFleur <dmitryzhigulin87@gmail.com>
2020-05-18 16:29:17 +0200

    done

--- Budget/script.js
+++ Budget/script.js
@@ -1,44 +1,39 @@
-
-
+// Бюджет на месяц + расчет бюджета на день в вычетом обязательных расходов.
 
 let budget = prompt("Ваш бюджет на месяц?");
 let date = prompt("Введите дату в формате YYYY-MM-DD");
 
-console.log(budget);
-console.log(date);
+// console.log(budget);
+// console.log(date);
 
 
 let appData = {
-
-    sum: '',
-    period: '',
+    sum: budget,
+    period: date,
     expenses : {},
     optionalExpenses : {},
     income : [],
     savings: false
 } ;
 
-
-appData["sum"] = budget;
-appData["period"] = date;
-
-console.log(appData);
+// console.log(appData);
 
 
 let expensesName = prompt("Введите обязательную статью расходов в этом месяце");
 let expensesBudget = prompt("“Во сколько обойдется?”");
 
-console.log(expensesName);
-console.log(expensesBudget);
-
-let expenses = {};
-expenses[expensesName] = expensesBudget;
+// console.log(expensesName);
+// console.log(expensesBudget);
 
 
+// добавляем значения в объект expenses.
+appData.expenses[expensesName] = expensesBudget; 
 
-appData["expenses"] = expenses;
 
+// высчитываем бюджет с учетом обязательных расходов
+appData.sum = appData.sum - expensesBudget;
 
-console.log(appData);
+// Высчитываем итоговый бюджет на день
+console.log(` Ваш бюджет на день составляет ${appData.sum / 30}`);
 
 
