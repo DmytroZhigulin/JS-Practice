8ac6c5d2ad981af19e0517fc99ab2ce1ba063464
Dmytro <dmitryzhigulin87@gmail.com>
2020-05-28 16:39:36 +0200

    Functions 1.0

--- Films/script.js
+++ Films/script.js
@@ -1,7 +1,17 @@
 "use strict";
 
-const numberOfFilms = +prompt("How much films you have watched?", "0");
+let numberOfFilms;
 let userRang;
+
+function start() {
+
+    numberOfFilms = +prompt("How much films you have watched?", "0");
+
+    while( numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt("Insert correct number please", "0");
+    }
+}
+start();
 
 
 let personalMovieDB = {
@@ -10,8 +20,11 @@ let personalMovieDB = {
     movies: {},
     actors: {},
     genres: [],
-    private: false,
+    private: true,
 };
+
+
+function rangUserCount() {
 
     if ( personalMovieDB.count >= 1 && personalMovieDB.count < 10 ) {
 
@@ -32,10 +45,19 @@ let personalMovieDB = {
         alert("ERROR");
         
     }
+}
+rangUserCount();
+
+    
 
 
 let filmName,
     filmRate;
+
+
+
+
+function rememberMyFilms() {
 
     for( let i = 0; i < 10;) {
 
@@ -57,23 +79,74 @@ let filmName,
   
     }
 
-    // do {
-    //     filmName = prompt("Last seen movie?");
-    //     filmRate = prompt("Rate last movie");
+        // do {
+        //     filmName = prompt("Last seen movie?");
+        //     filmRate = prompt("Rate last movie");
 
-    //     if ( filmName != null && filmRate != null && filmName != '' && filmRate != '' && filmName.length < 50 ) {
+        //     if ( filmName != null && filmRate != null && filmName != '' && filmRate != '' && filmName.length < 50 ) {
 
-    //         personalMovieDB.movies[filmName] = filmRate;
-    //         console.log("Nice!!!!")
+        //         personalMovieDB.movies[filmName] = filmRate;
+        //         console.log("Nice!!!!")
 
-    //     } else {
-    //         console.log("ERRRORRRRRRRRR!")
-    //     }
-            
-    // } while ( confirm("Add one more movie?") === true );
+        //     } else {
+        //         console.log("ERRRORRRRRRRRR!")
+        //     }
+                
+        // } while ( confirm("Add one more movie?") === true );
+}
+rememberMyFilms();
 
 
-console.log(personalMovieDB);
+
+let myGenre;
+let genreNum = 1;
+
+
+function writeYourGenres() {
+
+    for ( let i = 0; i < 3; i++) {
+
+        myGenre = prompt(`Your favorite genre number ${genreNum}`);
+        // вместо genreNum можно указать просто i, i = 1; i <=3; i++.
+
+        if (myGenre != '' && myGenre != null) {
+
+            console.log("GOOD");
+            personalMovieDB.genres.push(myGenre);
+            genreNum++; // увеличиваем значение переменной на 1
+
+        } else {
+
+            alert("Please, enter genre");
+            i--; //Возвращаемся на шаг назад
+        }
+    }
+}
+writeYourGenres();
+
+
+
+
+
+    
+function showMyDB() {
+    personalMovieDB.private === false ? console.log(personalMovieDB) : console.log("This is a privat account");
+}
+showMyDB();
+
+// Или так
+// function showMyDB (hidden) {
+//     if (!hidden) {
+//         console.log(personalMovieDB)
+//     } else {
+//         console.log("This is a privat account");
+//     }
+// }
+// showMyDB(personalMovieDB.private)
+
+
+
+// console.log(personalMovieDB);
 
 
     
